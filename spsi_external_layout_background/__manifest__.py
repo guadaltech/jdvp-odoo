61e05c7a207f7dab58554d67245cb1a3dd7b08d9
SPSI <spsisys@gmail.com>
2019-04-08 11:00:19 +0200

    empty background image & scss

--- spsi_external_layout_background/__manifest__.py
+++ spsi_external_layout_background/__manifest__.py
@@ -1,23 +1,22 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "spsi_external_layout_background",
+    'name': "SPSI External layout background",
+    'version': '0.1',
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Remove default background from 1st Document Template""",
 
     'description': """
-        Long description of module's purpose
+        Remove default background from Document Template (header and footer layout)
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': 'SPSI Sistemas',
+    'company': 'SPSI Sistemas',
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
@@ -25,11 +24,12 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        # 'views/views.xml',
+        'views/external_layout_background_view.xml',
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    'images': ['static/description/Logo-256.png'],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
 }
