5da406a117d4f2869dd13f447ef3027a8f4b99d8
Yunesky <yunesky.delrio@guadaltech.es>
2020-04-01 17:30:32 +0200

    clean modules

---
+++ l10n_es_vat_book_fix/__manifest__.py
@@ -0,0 +1,15 @@
+# See README.rst file on addon root folder for license details
+
+{
+    "name": "Libro de IVA-Fix",
+    "version": "12.0.1.3.0",
+    "author": "Yunesky",
+    "license": "AGPL-3",
+    "category": "Accounting",
+    "depends": [
+        'l10n_es_vat_book',
+    ],
+    'data': [
+    ],
+    "installable": True,
+}
