a59f7fc46bf799b707a8d0e0b6c900756ba37262
Yunesky <yunesky.delrio@guadaltech.es>
2020-04-02 11:17:42 +0200

    Change mrp bom domain in sales ordes

--- gst_sale_order_mrp/model/sale.py
+++ gst_sale_order_mrp/model/sale.py
@@ -20,17 +20,17 @@ class SaleOrderLine(models.Model):
         compute='_compute_bom_ids'
     )
 
-    @api.depends('order_id.partner_id')
     def _compute_bom_ids(self):
         for line in self:
             part_id = line.order_id.partner_id
+            line.bom_ids = line.product_id.mapped('bom_ids')
             if part_id:
                 bom_ids = part_id.mapped('partner_bom_ids')
                 bom_ids = bom_ids.filtered(lambda a:
                                            a.product_tmpl_id.id ==
                                            line.product_id.product_tmpl_id.id)
-                line.bom_ids = bom_ids.mapped('bom_id')
-
+                if bom_ids:
+                    line.bom_ids = bom_ids.mapped('bom_id')
 
     @api.multi
     @api.onchange('product_id')
@@ -39,11 +39,15 @@ class SaleOrderLine(models.Model):
         if not partner_id:
             raise UserError(_("Please, select the partner."))
         res = super(SaleOrderLine, self).product_id_change()
-        if res.get('domain', False):
+        if res.get('domain', False) and self.product_id:
             bom_ids = partner_id.mapped('partner_bom_ids')
             bom_ids = bom_ids.filtered(lambda a:
                                        a.product_tmpl_id.id ==
                                        self.product_id.product_tmpl_id.id)
-            res['domain'].update({'bom_id': [('id', 'in',
-                                              bom_ids.mapped('bom_id').mapped('id'))]})
+            if bom_ids:
+                dom = [('id', 'in', bom_ids.mapped('bom_id').mapped('id'))]
+            else:
+                bom_ids = self.product_id.mapped('bom_ids').mapped('id')
+                dom = [('id', 'in', bom_ids)]
+            res['domain'].update({'bom_id': dom})
         return res
